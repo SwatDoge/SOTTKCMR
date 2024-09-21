5d3ddf492c05ecdb274c0a7cb1207ddcf7471ad6
SwatDoge <bob.dedebouwer@hotmail.com>
2024-09-21 15:27:31 +0000

    Create userscript.user.js

---
+++ userscript.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Codemirror rerouter
+// @description  Reroutes to codemirror mirror without cors
+// @author       Swat
+// @match        https://krunker.io/editor.html
+// @match        https://krunker.io/scripting.html
+// @updateURL    https://github.com/SwatDoge/SOTT-Quick-Krunkscript-Docs/raw/main/tampermonkey/userscript.user.js
+// @downloadURL  https://github.com/SwatDoge/SOTT-Quick-Krunkscript-Docs/raw/main/tampermonkey/userscript.user.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+new MutationObserver(async (mutations, observer) => {
+    mutations.forEach(el => el.addedNodes.forEach(node => node.src &&= node.src.replace("codemirror.net", "codemirror-mirror.swatdo.ge")));
+}).observe(document, {
+    childList: true,
+    subtree: true,
+})
